367b01ee9a280298317ed2f861dc442c9f214949
eNoRasfiwljnfakljfghdshglskdfjcm <92513899+ennnnnoooorrrrrraaaaaa@users.noreply.github.com>
2021-10-28 15:52:23 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,18 @@
+/**
+*这是一个打印helloworld的文档
+*@author enora
+*@version 1.0.0
+ */
+public class test{
+    public static void main(String[] args){
+        //打印helloworld
+        System.out.println("hello world");
+        /*
+        java
+        is the
+        best
+        */
+        System.out.print("what's wrong?");
+        System.out.print("Here I am!");
+    }
+}
